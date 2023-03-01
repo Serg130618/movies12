fa25adf792c2decc880eedcf340d8e85a6029f3d
ss13 <ssa0618@yandex.ru>
2023-03-01 14:06:22 +0300

    1

---
+++ src/main/java/MoviesMan.java
@@ -0,0 +1,34 @@
+public class MoviesMan {
+    private String[] movies = new String[0];
+    private int limit;
+    public MoviesMan() {
+        this.limit = 10;
+    }
+    public MoviesMan(int limit) {
+        this.limit = limit;
+    }
+    public void add(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+    public String[] findAll() {
+        return movies;    }
+    public String[] findLast() {
+        int resultLenght;
+        if (movies.length < limit) {
+            resultLenght = movies.length;
+        } else {
+            resultLenght = limit;
+        }
+        String[] tmp = new String[resultLenght];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+
+        }
+        return tmp;
+    }
+}
---
+++ src/test/java/MoviesTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoviesTest {
+    @Test
+    public void test() {
+        MoviesMan man = new MoviesMan();
+
+        man.add("фильм 1");
+        man.add("фильм 2");
+        man.add("фильм 3");
+
+        String[] expected = { "фильм 1", "фильм 2", "фильм 3"};
+        String[] actual = man.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        MoviesMan man = new MoviesMan();
+
+        man.add("фильм 1");
+        man.add("фильм 2");
+        man.add("фильм 3");
+
+        String[] expected = { "фильм 3", "фильм 2", "фильм 1"};
+        String[] actual = man.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        MoviesMan man = new MoviesMan(10);
+
+        man.add("фильм 1");
+        man.add("фильм 2");
+        man.add("фильм 3");
+        man.add("фильм 4");
+        man.add("фильм 5");
+        man.add("фильм 6");
+        man.add("фильм 7");
+        man.add("фильм 8");
+        man.add("фильм 9");
+        man.add("фильм 10");
+        man.add("фильм 11");
+        man.add("фильм 12");
+
+        String[] expected = { "фильм 12", "фильм 11", "фильм 10", "фильм 9","фильм 8","фильм 7","фильм 6","фильм 5", "фильм 4","фильм 3"};
+        String[] actual = man.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
